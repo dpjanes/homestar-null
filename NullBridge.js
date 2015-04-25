6018e36b7d82d3b92098fdd9622e5e99ff5c70de
David Janes <davidjanes@davidjanes.com>
2015-04-25 11:23:09 -0400

    validate push

--- NullBridge.js
+++ NullBridge.js
@@ -117,6 +117,8 @@ NullBridge.prototype.push = function (pushd) {
         return;
     }
 
+    self._validate_push(pushd);
+
     logger.info({
         method: "push",
         unique_id: self.unique_id,
